5bb5424355a5ab4ad6f446eaa0ea45599305a60e
sunhaipeng2 <sunhaipeng@jd.com>
2020-01-14 17:58:06 +0800

    初始化

---
+++ src/main/java/com/test11/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.test11.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        String json = "{\"name\":\"1231\",\"age\":123141}";
+        return "hello world";
+    }
+}
